b34a4ad355bc3cec5cfe486bdc6b5d37af6cf8e5
Mecopi <mecopilive@gmail.com>
2023-02-13 12:06:08 +0100

    feat(common): add users ressources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompteModule } from './compte/compte.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CompteModule],
+  imports: [CompteModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
