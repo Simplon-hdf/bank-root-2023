60727eed377de7013dc030cbc269a07e7c234413
delannoy kurt <delannoykurt666@gmail.com>
2023-02-14 15:45:24 +0100

    test

--- src/compte/dto/create-compte.dto.ts
+++ src/compte/dto/create-compte.dto.ts
@@ -1,1 +1,27 @@
-export class CreateCompteDto {}
+import {Admin, Client, Transaction} from "@prisma/client";
+import {IsDate} from "class-validator";
+
+const { String } = require('class-validator' );
+
+export class CreateCompteDto {
+    @String()
+    private idAccount: string;
+    @String()
+    private idAdmin: string;
+    @String()
+    private ccountNumber: string;
+    // Client
+    @String()
+    private idClient: string;
+    @IsDate()
+    private createdAt: Date;
+    @IsDate()
+    private updatedAt: Date;
+
+    private client: Client;
+
+    private admin: Admin;
+
+    private transactions: Transaction[];
+    ``
+}
