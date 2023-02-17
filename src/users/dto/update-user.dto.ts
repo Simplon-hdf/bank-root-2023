68322a497168cd83ae8710b6768cc29756f42825
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-17 10:16:24 +0100

    feat(common): add user update dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,26 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+import { CreateUserDto } from "./create-user.dto";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  lastname: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  firsname: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  email_address: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
