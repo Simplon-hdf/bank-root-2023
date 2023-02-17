dc43d52be85d05aebc8837b3d485ee33dc3ad535
Josue <96113041+Rowada@users.noreply.github.com>
2023-02-17 11:37:03 +0100

    feat(common): add swagger in admin.controller

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AdminService } from './admin.service';
-import { CreateAdminDto } from './dto/create-admin.dto';
-import { UpdateAdminDto } from './dto/update-admin.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { AdminService } from "./admin.service";
+import { CreateAdminDto } from "./dto/create-admin.dto";
+import { UpdateAdminDto } from "./dto/update-admin.dto";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
-@Controller('admin')
+@Controller("admin")
+@ApiTags("Admin")
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Post()
+  @Post("create-admin")
+  @ApiOperation({ summary: "Create Admin" })
   create(@Body() createAdminDto: CreateAdminDto) {
     return this.adminService.create(createAdminDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "Get All Admins" })
   findAll() {
     return this.adminService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  @ApiOperation({ summary: "Get Admin by Id" })
+  findOne(@Param("id") id: string) {
     return this.adminService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
+  @Patch(":id")
+  @ApiOperation({ summary: "Update Admin" })
+  update(@Param("id") id: string, @Body() updateAdminDto: UpdateAdminDto) {
     return this.adminService.update(+id, updateAdminDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  @ApiOperation({ summary: "Delete Admin" })
+  remove(@Param("id") id: string) {
     return this.adminService.remove(+id);
   }
 }
