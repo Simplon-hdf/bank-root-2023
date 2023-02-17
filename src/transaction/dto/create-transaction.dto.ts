d6d5254411abbc20f8cc933e9a2aaa79378b1740
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-17 14:26:38 +0100

    modification on create-transaction.dto

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,35 @@
-export class CreateTransactionDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, isDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+
+export class CreateTransactionDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    uuid_transaction: string;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    id_transaction: number;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    account_id: number;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    type: string;
+
+    @ApiProperty()
+    @IsDate()
+    @IsNotEmpty()
+    created_at: Date;
+
+    @ApiProperty()
+    @IsDate()
+    @IsNotEmpty()
+    updated_at: Date;
+}
