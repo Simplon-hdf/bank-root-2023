5706665278aff9f5b618d4175a26b9bcf846ec8a
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-17 10:07:47 +0100

    feat(common): add create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,34 @@
-import { Client } from "@prisma/client";
-import {IsNumber, IsString} from "class-validator"
+import { IsNumber, IsString, IsNotEmpty } from "class-validator"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class CreateUserDto {
-@IsNumber()    
-private idClient: number;
+@ApiProperty()
 @IsString()
-private lastName: string;
-@IsString()
-private firstName: string;
-@IsString()
-private emailAddress: string;
-@IsString()
-private password: string;
+@IsNotEmpty()
+uuid_client: string;
 
+@ApiProperty()
+@IsNumber()
+@IsNotEmpty()
+id_client: number;
+
+@ApiProperty()
+@IsString()
+@IsNotEmpty()
+lastname: string;
+
+@ApiProperty()
+@IsString()
+@IsNotEmpty()
+firstname: string;
+
+@ApiProperty()
+@IsString()
+@IsNotEmpty()
+email_address: string;
+
+@ApiProperty()
+@IsString()
+@IsNotEmpty()
+password: string;
 }
