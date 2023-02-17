207d5d20a53255fbbc3fa9a9a7e5c00bd710d841
Josue <96113041+Rowada@users.noreply.github.com>
2023-02-17 22:56:43 +0100

    feat(prisma): update schema prisma

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,35 +1,34 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDate, isDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
-
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateTransactionDto {
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    uuid_transaction: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  uuid_transaction: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    id_transaction: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  id_transaction: number;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    account_id: number;
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  account_id: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    type: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  type: string;
 
-    @ApiProperty()
-    @IsDate()
-    @IsNotEmpty()
-    created_at: Date;
+  @ApiProperty()
+  @IsDate()
+  @IsNotEmpty()
+  created_at: Date;
 
-    @ApiProperty()
-    @IsDate()
-    @IsNotEmpty()
-    updated_at: Date;
+  @ApiProperty()
+  @IsDate()
+  @IsNotEmpty()
+  updated_at: Date;
 }
