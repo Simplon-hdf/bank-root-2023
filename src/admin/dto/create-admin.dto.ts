8c7f1df6dc56799d937f0e82d6e71f70362fc51a
Elodie mongellaz <mongellaz.e@gmail.com>
2023-02-17 10:40:26 +0100

    feat(common): add create admin dto

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,1 +1,36 @@
-export class CreateAdminDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+
+export class CreateAdminDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    uuid_admin: string;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    id_admin: number;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    lastname: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    firstname: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    email_address: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    password: string;   
+}
+
