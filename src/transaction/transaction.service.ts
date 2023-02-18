17f75c6135b442b397f733a66673e9604a6bcfb9
delannoy kurt <delannoykurt666@gmail.com>
2023-02-18 14:31:58 +0100

    feat(transaction): add methods, creatTransaction, getTransactionForAccount

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -2,85 +2,81 @@ import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { PrismaService } from "src/prisma/prisma.service";
-import { CreateCompteDto } from "src/compte/dto/create-compte.dto";
-import {Account, Prisma, Transaction} from "@prisma/client";
+import { PrismaClient, Transaction } from "@prisma/client";
 
 
-async function isExist(id: number) {
-    const accountNumber = await this.prisma.account.findOne({
-        where: {
-            id_account: id
-        }
-    });
-    // check if account exist
-    try {
-        if (accountNumber) {
-            return true;
-        }
-    } catch (err) {
-        console.log(err.log("Account not found"));
-        return false;
-    }
-}
+const prisma = new PrismaClient();
 
-async function isNotNullAndSuperior(amount: number, sum: number) {
-    // check if account is positive
-    try {
-        if (amount > 0 && amount > sum) {
-            return true;
-        }
-    } catch (err) {
-        console.log(err.log("amount of account not positive or not authorized"));
-        return false;
-    }
-}
 
 @Injectable()
 export class TransactionService {
 
     constructor(private readonly prisma: PrismaService) {}
-  async createDepot(createTransactionDto: CreateTransactionDto, sum: number): Promise<Transaction>{
-    const account = await (createTransactionDto.account_id);
 
-      return this.prisma.transaction.create({
-        data: {
-            amount: sum,
-            created_at: new Date(Date.now()),
+
+
+    async createTransaction(createTransactionDto: CreateTransactionDto) {
+        const { account_id, amount, type } = createTransactionDto;
+
+        const account = await prisma.account.findUnique({
+            where: { id_account: account_id },
+        });
+
+        if (!account) {
+            throw new Error(`No account found with id ${account_id}`);
         }
 
-    });
-  }
+        if (type === 'debit' && amount > account.balance) {
+            throw new Error('Insufficient funds');
+        }
 
+        const transaction = await prisma.transaction.create({
+            data: {
+                account: { connect: { id_account: account_id } },
+                amount,
+                type,
+            },
+        });
 
-  createRetrait(createTransactionDto: CreateTransactionDto) {
-    isNotNullAndSuperior(// solde du compte, somme);
-    // requete avec prisma
-  }
+        if (type === 'debit') {
+            await prisma.account.update({
+                where: { id_account: account_id },
+                data: { balance: account.balance - amount },
+            });
+        } else if (type === 'credit') {
+            await prisma.account.update({
+                where: { id_account: account_id },
+                data: { balance: account.balance + amount },
+            });
+        }
 
-  // @ts-ignore
-    createVirement(createTransactionDto: CreateTransactionDto) {
-    // verif le solde du compte
-    isNotNullAndSuperior(// solde du compte, somme);
-    // verif le compte de destination existe
-    //isExist();
+        return transaction;
+    }
 
-  }
+    async getTransactionsForAccount(accountId: number) {
+        const transactions = await prisma.transaction.findMany({
+            where: { account_id: accountId },
+            orderBy: { created_at: 'desc' },
+        });
 
+        return transactions;
+    }
 
+    // ----------------------------------------------------------------
+    findAll() {
+        return `This action returns all transaction`;
+    }
 
-  findAll() {
-    return `This action returns all transaction`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} transaction`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
-  }
+    update(id: number, updateTransactionDto: UpdateTransactionDto) {
+        return `This action updates a #${id} transaction`;
+    }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} transaction`;
+    }
 }
+
