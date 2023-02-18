a961d93d9ca746a711ec6dd208d49044c6320422
Josue <96113041+Rowada@users.noreply.github.com>
2023-02-18 15:34:13 +0100

    fix(transaction): debug transaction.service

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,82 +1,78 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateTransactionDto } from "./dto/create-transaction.dto";
+import { UpdateTransactionDto } from "./dto/update-transaction.dto";
 import { PrismaService } from "src/prisma/prisma.service";
-import { PrismaClient, Transaction } from "@prisma/client";
-
+import { PrismaClient, Transaction, Account } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 @Injectable()
 export class TransactionService {
+  constructor(private readonly prisma: PrismaService) {}
 
-    constructor(private readonly prisma: PrismaService) {}
+  async createTransaction(
+    createTransactionDto: CreateTransactionDto
+  ): Promise<Transaction> {
+    const { account_id, amount, type } = createTransactionDto;
 
+    const account = await prisma.account.findUnique({
+      where: { id_account: account_id },
+    });
 
-
-    async createTransaction(createTransactionDto: CreateTransactionDto) {
-        const { account_id, amount, type } = createTransactionDto;
-
-        const account = await prisma.account.findUnique({
-            where: { id_account: account_id },
-        });
-
-        if (!account) {
-            throw new Error(`No account found with id ${account_id}`);
-        }
-
-        if (type === 'debit' && amount > account.balance) {
-            throw new Error('Insufficient funds');
-        }
-
-        const transaction = await prisma.transaction.create({
-            data: {
-                account: { connect: { id_account: account_id } },
-                amount,
-                type,
-            },
-        });
-
-        if (type === 'debit') {
-            await prisma.account.update({
-                where: { id_account: account_id },
-                data: { balance: account.balance - amount },
-            });
-        } else if (type === 'credit') {
-            await prisma.account.update({
-                where: { id_account: account_id },
-                data: { balance: account.balance + amount },
-            });
-        }
-
-        return transaction;
+    if (!account) {
+      throw new Error(`No account found with id ${account_id}`);
     }
 
-    async getTransactionsForAccount(accountId: number) {
-        const transactions = await prisma.transaction.findMany({
-            where: { account_id: accountId },
-            orderBy: { created_at: 'desc' },
-        });
-
-        return transactions;
+    if (type === "debit" && amount > account.balance) {
+      throw new Error("Insufficient funds");
     }
 
-    // ----------------------------------------------------------------
-    findAll() {
-        return `This action returns all transaction`;
+    const transaction = await prisma.transaction.create({
+      data: {
+        account: { connect: { id_account: account_id } },
+        amount,
+        type,
+      },
+    });
+
+    if (type === "debit") {
+      await prisma.account.update({
+        where: { id_account: account_id },
+        data: { balance: account.balance - amount },
+      });
+    } else if (type === "credit") {
+      await prisma.account.update({
+        where: { id_account: account_id },
+        data: { balance: account.balance + amount },
+      });
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} transaction`;
-    }
+    return transaction;
+  }
 
-    update(id: number, updateTransactionDto: UpdateTransactionDto) {
-        return `This action updates a #${id} transaction`;
-    }
+  async getTransactionsForAccount(accountId: number) {
+    const transactions = await prisma.transaction.findMany({
+      where: { account_id: accountId },
+      orderBy: { created_at: "desc" },
+    });
 
-    remove(id: number) {
-        return `This action removes a #${id} transaction`;
-    }
+    return transactions;
+  }
+
+  // ----------------------------------------------------------------
+  findAll() {
+    return `This action returns all transaction`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} transaction`;
+  }
+
+  update(id: number, updateTransactionDto: UpdateTransactionDto) {
+    return `This action updates a #${id} transaction`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} transaction`;
+  }
 }
-
